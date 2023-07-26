1b2655965470a477daa5a57594bdd7649fe4df1c
Camille Duprez <launchmesomegames@gmail.com>
2023-07-26 11:06:20 +0200

    Ajout models et routes

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Camille:<qv0O8ZiAmqqMZyGF>@cluster0.hfysfuz.mongodb.net/?retryWrites=true&w=majority',
+
+const authRoutes = require('./routes/auth');
+const booksRoutes = require('./routes/books');
+
+mongoose.connect('mongodb+srv://Camille:qv0O8ZiAmqqMZyGF@cluster0.hfysfuz.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-const express = require('express');
-
 const app = express();
-
-app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
+});
 
-app.get('/api/books', (req, res, next) => {
-    const books = [];
-    res.status(200).json(books);
-  });
+app.use(bodyParser.json());
+
+app.use('api/auth', authRoutes);
+app.use('api/books', booksRoutes);
 
 module.exports = app;
