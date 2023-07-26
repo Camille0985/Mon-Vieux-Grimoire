93ec3c8c98bd008004b3f5d7255f58eb5e0bcda2
Camille Duprez <launchmesomegames@gmail.com>
2023-07-26 18:34:44 +0200

    Ajout routes books

--- routes/books.js
+++ routes/books.js
@@ -1,4 +1,6 @@
 const express = require('express');
+//const auth = require('auth');
+
 const router = express.Router();
 
 const bookCtrl = require('../controllers/books');
@@ -7,10 +9,14 @@ router.get('/', bookCtrl.getAllBooks);
 
 router.get('/:id', bookCtrl.getOneBook);
 
+router.get('/bestrating', bookCtrl.getBestBooks);
+
 router.post('/', bookCtrl.createBook);
 
 router.put('/:id', bookCtrl.modifyBook);
 
 router.delete('/:id', bookCtrl.deleteBook);
+
+//router.post('/:id/rating', auth, bookCtrl.rateOneBook);
 
 module.exports = router;
